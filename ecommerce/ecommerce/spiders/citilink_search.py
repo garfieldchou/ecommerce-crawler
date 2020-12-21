3b11d73fd3ff0db66e7c430c0ad128a3d2959308
garfield <cmgarfield@gmail.com>
2020-12-21 14:57:17 +0800

    Parse title, rating, price, and spec details

--- ecommerce/ecommerce/spiders/citilink_search.py
+++ ecommerce/ecommerce/spiders/citilink_search.py
@@ -6,15 +6,27 @@ from scrapy.spiders import CrawlSpider, Rule
 class CitilinkSearchSpider(CrawlSpider):
     name = 'citilink_search'
     allowed_domains = ['www.citilink.ru']
-    start_urls = ['http://www.citilink.ru/']
+    start_urls = ['https://www.citilink.ru/search/?text=Gaming+notebook']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//div[@class='ProductCardVertical__description ']/a",
+                           process_value=lambda link: link + 'properties'),
+             callback='parse_item',
+             follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        spec_dict = {}
+        specs = response.xpath("//div[@class='Specifications__row']")
+        for spec in specs:
+            key = spec.xpath(
+                ".//div[@class='Specifications__column Specifications__column_name']/text()").get().strip()
+            spec_dict[key] = spec.xpath(
+                ".//div[@class='Specifications__column Specifications__column_value']/text()").get().strip()
+
+        yield {
+            'title': response.xpath("normalize-space(//h1/text())").get(),
+            'rating': response.xpath("normalize-space(//span[@class=' IconWithCount__count js--IconWithCount__count']/text())").get(),
+            'price': response.xpath("normalize-space(//span[@class='ProductHeader__price-default_current-price']/text())").get(),
+            'spec': spec_dict
+        }
