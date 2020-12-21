7d9012e6b29e32b6e92fcffeedd13041b795d60f
garfield <cmgarfield@gmail.com>
2020-12-21 10:14:46 +0800

    Retrieve title and price for each item

--- ecommerce/ecommerce/spiders/kakaku.py
+++ ecommerce/ecommerce/spiders/kakaku.py
@@ -6,15 +6,17 @@ from scrapy.spiders import CrawlSpider, Rule
 class KakakuSpider(CrawlSpider):
     name = 'kakaku'
     allowed_domains = ['kakaku.com']
-    start_urls = ['http://kakaku.com/']
+    start_urls = [
+        'https://kakaku.com/search_results/gaming%20laptop/?act=Input&lid=pc_ksearch_searchbutton_top']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//a[@class='p-item_visual is-biggerlinkBigger s-biggerlinkHover_alpha']",
+                           process_value=lambda link: link.replace('?lid=pc_ksearch_kakakuitem', 'spec')),
+             callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'title': response.xpath("//h2/text()").get(),
+            'price': response.xpath("//span[@class='priceTxt']/text()").get()
+        }
